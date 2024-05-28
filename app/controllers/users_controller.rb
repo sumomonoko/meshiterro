57ec248278b03fca81535ead1e11a65cb8e22098
shiori saitou <shi.o.rin.5082@gmail.com>
2024-05-28 14:36:37 +0000

    [Add]User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
